d454b26f04675845aa9f403bbfb15cf758638539
Aford <244734834@qq.com>
2018-01-08 17:12:13 +0800

    🚧 Modified Details

--- src/libs/focus.js
+++ src/libs/focus.js
@@ -2,7 +2,7 @@ const appTitle = document.title
 document.addEventListener('visibilitychange', () => {
   const isHidden = document.hidden
   if (isHidden) {
-    document.title = `😴 ${appTitle}`
+    document.title = `😴HALO - Waiting For You`
   } else {
     document.title = appTitle
   }
