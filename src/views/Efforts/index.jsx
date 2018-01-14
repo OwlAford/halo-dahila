112c1eec9e64e17de686f27042dab2db62a53815
Aford <244734834@qq.com>
2018-01-14 21:57:59 +0800

    Add Empty Component

---
+++ src/views/Efforts/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './scss/index.scss'
+
+export default class Efforts extends React.Component {
+  componentWillMount () {
+  }
+
+  render () {
+    return (
+      <div>
+        effort
+      </div>
+    )
+  }
+}
