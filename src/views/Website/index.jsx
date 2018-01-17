15b35fdbfdb63a73fc408e14ff0e5a6a7d6550bc
Aford <244734834@qq.com>
2018-01-17 20:29:32 +0800

    🐱 Upgrade packages

---
+++ src/views/Website/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './scss/index.scss'
+
+export default class Website extends React.Component {
+  componentWillMount () {
+  }
+
+  render () {
+    return (
+      <div className='home-website'>
+        Website
+      </div>
+    )
+  }
+}
