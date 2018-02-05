d2b45b521f52fd14fa032ceda3f711d4c564ffd1
Aford <244734834@qq.com>
2018-02-05 12:58:43 +0800

    Online detective

--- src/constants/connect.js
+++ src/constants/connect.js
@@ -22,3 +22,20 @@ export const getData2Array = (path, callback) => {
     callback(arr)
   })
 }
+
+export const online = (name, avatar) => {
+  const onlineRef = rootPath + 'chatRoom/online'
+  const { cname, cip } = window.returnCitySN
+  let item = {}
+  let curKey = '/' + cip.replace(/\./g, '_')
+  item[curKey] = {
+    cip,
+    cname,
+    name,
+    avatar
+  }
+  WD.sync().ref(onlineRef).update(item)
+  WD.sync().ref(onlineRef + curKey).onDisconnect().remove()
+}
+
+online('Aford', 'owl')
