e97c46594d33dbdcd83cbaae275660a8137dde1e
OwlAford <244734834@qq.com>
2018-04-12 09:59:13 +0800

    🔧[Daily bug fixes]

--- config/style-loader.js
+++ config/style-loader.js
@@ -21,6 +21,19 @@ const styleLoader = (loader, options) => {
         require('postcss-import')({
           root: loader.resourcePath
         }),
+        require('postcss-custom-properties')(),
+        require('postcss-custom-media')(),
+        require('postcss-media-minmax')(),
+        require('postcss-custom-selectors')(),
+        require('postcss-calc')(),
+        require('postcss-nesting')(),
+        require('postcss-nested')(),
+        require('postcss-color-function')(),
+        require('pleeease-filters')(),
+        require('pixrem')(),
+        require('postcss-selector-matches')(),
+        require('postcss-selector-not')(),
+        require('postcss-flexbugs-fixes')(),
         require('autoprefixer')({
           browsers: packageConfig.browserslist
         }),
